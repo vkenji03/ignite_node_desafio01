5d5b153e8ac651cd186ed3ca2eba32c9b28cd021
Vitor Kenji <viitor.kenji@gmail.com>
2022-05-15 17:35:49 -0300

    finish route post/users and its tests(users.spec.js)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+// object user = {
+//  id: uuidv4(),
+//  name,
+//  username,
+//  todos: []
+// }
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userExists = users.some(user => user.username === username);
+  if (userExists) {
+    return response.status(400).json({ error: 'Username already being used' });
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  };
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
