394ea46e7a333a66b39705a0744d555deb278a1d
Vitor Kenji <viitor.kenji@gmail.com>
2022-05-17 02:45:21 -0300

    finish route patch/todo/:id/done

--- src/index.js
+++ src/index.js
@@ -90,7 +90,18 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(user => user.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found' })
+  }
+
+  todo.done = true;
+
+  return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
